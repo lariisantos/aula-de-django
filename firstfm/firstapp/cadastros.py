fd859b848c2ba1a955ca957b38efe513dec36d47
Larissa Santos <lari.santos.sobrinho@gmail.com>
2024-06-26 10:25:23 -0300

    cadastro

---
+++ firstfm/firstapp/cadastros.py
@@ -0,0 +1,56 @@
+from firstapp.models import Album, Musica
+
+class CadastroAlbums:
+
+    def criar(titulo, artista, genero):
+        a = Album()
+        a.titulo = titulo
+        a.artista = artista
+        a.genero = genero
+        a.save()
+        return a
+    
+    def obter(id):
+        return Album.objects.filter(id=id).first()
+    
+    def excluir(id):
+        a = CadastroAlbums.obter(id)
+        a.delete()
+    
+    def atualizar(id, titulo, artista, genero):
+        a = CadastroAlbums.obter(id)
+        if a is None:
+            raise ValueError('Álbum não existe: id= ',id)
+        a.titulo = titulo
+        a.artista = artista
+        a.genero = genero
+        a.save()
+
+class CadastroMusicas:
+
+    def criar(titulo, compositor, duracao, album_id):
+        m = Musica()
+        m.titulo = titulo
+        m.compositor = compositor
+        m.duracao = duracao
+        m.album = CadastroAlbums.obter(album_id)
+        m.save()
+        return m
+    
+    def obter(id):
+        return Musica.objects.filter(id=id).first()
+    
+    def excluir(id):
+        m = CadastroMusicas.obter(id)
+        m.delete()
+    
+    def atualizar(id, titulo, compositor, duracao, album_id):
+        m = CadastroMusicas.obter(id)
+        if m is None:
+            raise ValueError('Música não existe: id= ',id)
+        m.titulo = titulo
+        m.compositor = compositor
+        m.duracao = duracao
+        m.album = CadastroAlbums.obter(album_id)
+        m.save()
+        return m
--- firstfm/firstfm/settings.py
+++ firstfm/firstfm/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'firstapp.apps.FirstappConfig',
+    'firstapp',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
